0ed848e6518600583a2e90dfd8ce45c8679f3279
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-08-09 12:48:18 -0300

    Starting to clean up code
      * Avoiding heavy nesting in deploy task (WIP)
      * Added basic instrumentation support
      * Replaced hardcoded paths with env vars defined in NAMELIST.environment

--- fabfile.py
+++ fabfile.py
@@ -2,79 +2,92 @@
 
 from __future__ import with_statement
 
-from fabric.api import run, local, cd, settings, show, env
+from fabric.api import run, local, cd, settings, show, env, prefix
 from fabric.colors import green, red, yellow
 from fabric.contrib.files import exists, comment
 from fabric.decorators import hosts
 
 
+
 @hosts('tupa')
-def deploy(instrument=False, code_dir='run',
-           submit_dir='$SUBMIT_DIR',
+def deploy(instrument=True,
+           code_dir='$HOME/run',
+           submit_dir='$SUBMIT_HOME',
            branch='monin'):
     print(green("Started"))
-    with cd(submit_dir):
-        check_code(code_dir, 'monin')
-        with cd(code_dir):
-            with cd('Modelos/AGCM-1.0/model'):
-                link_agcm_inputs()
-            with cd('Modelos/MOM4p1/exp/cpld2.1'):
-                replace_vars()
+    check_code(code_dir, branch, clean=False)
+    with cd(code_dir):
+        with cd('Modelos/MOM4p1/exp/cpld2.1'):
+            replace_vars()
+            with prefix('source NAMELIST.environment'):
                 if instrument:
-                    prepare_instrumentation()
-                    compile_model()
-                    instrument_code()
+                    with prefix('module load perftools'):
+#                        clean_model_compilation()
+                        compile_model()
+                        instrument_code()
                 else:
                     compile_model()
+                prepare_expdir()
+                run('mkdir -p ${rootexp}/AGCM-1.0/model')
+                link_agcm_inputs()
                 prepare_workdir()
-                run_model()
-
-
-def prepare_instrumentation():
-    print(yellow('Preparing instrumentation'))
-    run('module load perftools')
+            fix_submit()
+            run_model()
 
 
 def instrument_code():
     print(yellow('Rebuilding executable with instrumentation'))
-    with cd('exec'):
-        run('pat_build -O apa ${executable}')
+    run('cp ~/pat/instrument_coupler.apa ${expdir}/exec/')
+    run('pat_build -O ${expdir}/exec/instrument_coupler.apa -o ${executable}+apa ${executable}')
+    run("sed -i.bak -r -e 's/^export executable=.*$/export executable=\$\{expdir\}\/exec\/fms_mom4p1_coupled.x+apa/g' NAMELIST.environment")
 
 
 def run_model():
     print(yellow('Running model'))
-    run('./run_g4c_model.cray cold 2007010100 2007020100 2007020100 48 rodteste')
+    run('. run_g4c_model.cray cold 2007010100 2007011000 2007011000 48 rodteste')
+
+
+def prepare_expdir():
+    print(yellow('Preparing expdir'))
+    run('cp -R tables ${expdir}')
 
 
 def prepare_workdir():
     print(yellow('Preparing workdir'))
-    run('mkdir -p $WORK_HOME/Modelos/MOM4p1/cpld2.1/')
-    run('cp -R $ARCHIVE_OCEAN/database/work20070101/ $WORK_HOME/Modelos/MOM4p1/cpld2.1/')
-    run('touch $WORK_HOME/Modelos/MOM4p1/cpld2.1/work20070101/time_stamp.restart')
+    run('mkdir -p ${workdir}')
+    run('cp -R $ARCHIVE_OCEAN/database/work20070101/* ${workdir}')
+    run('touch ${workdir}/time_stamp.restart')
+
+
+def clean_model_compilation():
+    print(yellow("Cleaning code dir"))
+    run('./run_g4c_model.cray clean')
 
 
 def compile_model():
     print(yellow("Compiling code"))
-    if not exists('exec'):
-        run('mkdir exec')
+    run('mkdir -p ${expdir}/exec')
     run('./run_g4c_model.cray make')
 
 
-def check_code(code_dir, branch):
+def check_code(code_dir, branch, clean=True):
     # FIXME: sanitize input!
     print(yellow("Checking code"))
+    if clean:
+        run('rm -rf %s' % code_dir)
     if not exists(code_dir):
         print(yellow("Creating new repository"))
-        run('hg clone $ARCHIVE_OCEAN/repos %s' %  code_dir)
+        run('hg clone $ARCHIVE_OCEAN/repos %s' % code_dir)
     with cd(code_dir):
         print(yellow("Updating existing repository"))
+        run('hg pull')
         run('hg update %s' % branch)
 
 
 def link_agcm_inputs():
-    if not exists('datain'):
+    if not exists('${rootexp}/AGCM-1.0/model/datain'):
         print(yellow("Linking AGCM input data"))
-        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain datain')
+        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain ${rootexp}/AGCM-1.0/model/datain')
 
 
 def replace_vars():
@@ -83,10 +96,10 @@ def replace_vars():
     print(yellow("Replacing variables in NAMELIST.environment"))
 
     print(yellow("Replacing variable root"))
-    run("sed -i.bak -r -e 's/^export root=.*$/export root=\$\{SUBMIT_HOME\}\/run\/Modelos/g' NAMELIST.environment")
+    run("sed -i.bak -r -e 's/^export root=.*$/export root=\$\{HOME\}\/run\/Modelos/g' NAMELIST.environment")
 
     print(yellow("Replacing variable rootexp"))
-    run("sed -i.bak -r -e 's/^export rootexp=.*$/export rootexp=\$\{SUBMIT_HOME\}\/run\/Modelos/g' NAMELIST.environment")
+    run("sed -i.bak -r -e 's/^export rootexp=.*$/export rootexp=\$\{SUBMIT_HOME\}\/Modelos/g' NAMELIST.environment")
 
     print(yellow("Replacing variable workdir"))
     run("sed -i.bak -r -e 's/^export workdir=.*$/export workdir=\$\{WORK_HOME\}\/Modelos\/MOM4p1\/\$\{name\}\/work20070101/g' NAMELIST.environment")
@@ -101,3 +114,7 @@ def replace_vars():
     run("sed -i.bak -r -e 's/^export datatable=.*$/export datatable=\$\{expdir\}\/tables\/data_table/g' NAMELIST.environment")
 
     run('touch tables/data_override')
+
+
+def fix_submit():
+    run("sed -i.bak -r -e 's/^aprun -n \$\{npes\} submit/aprun -n \$\{npes\} .\/submit/g' run_g4c_model.cray")
