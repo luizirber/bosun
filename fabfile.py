3bd10d2dba2eff058753ccf96121c0a39bbd1377
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-09-05 17:12:51 -0300

    Working with revision 97 model code.
        * Replaced Beto replace function with mine, I'm doing something wrong with
          his version and it is not working.
        * Avoiding as much as possible shell env vars, but the current scripts still
          depends too much on them. At least all commands sent to server are
          pre-formatted before sending.
        * Added a new fix, the current script sources a NAMELIST.env and replaces
          shell envs defined on new script. Just commenting the line solves the
          problem.
        * Still need to fix post-processing!

--- fabfile.py
+++ fabfile.py
@@ -13,7 +13,6 @@ import yaml
 
 def shell_env(args):
     env_vars = " ".join(["=".join((key, str(value))) for (key, value) in args.items()])
-    print env_vars
     return prefix("export %s" % env_vars)
 
 
@@ -38,39 +37,75 @@ def deploy(filename='namelist.yaml', instrument=False):
 
 
 def _read_config(filename):
+
+
+    def rec_replace(env, key, value):
+        finder = re.compile('\$\{(\w*)\}')
+        ret_value = value
+
+        try:
+            keys = finder.findall(value)
+        except TypeError:
+            return value
+
+        for k in keys:
+            if k in env:
+                ret_value = rec_replace(env, k, ret_value.replace('${%s}' % k, env[k]))
+
+        return ret_value
+
+
+    def env_replace(old_env):
+        new_env = {}
+        for k in old_env.keys():
+            try:
+                old_env[k].split(' ')
+            except AttributeError:
+                new_env[k] = old_env[k]
+            else:
+                new_env[k] = rec_replace(old_env, k, old_env[k])
+        return new_env
+
+
+
     data = open(filename, 'r').read()
     TOKEN = re.compile(r'''\$\{(.*?)\}''')
 
     d = yaml.load(data)
-    while set(TOKEN.findall(data)) & set(d.keys()):
-        d = yaml.load(data)
-        data = TOKEN.sub(lambda m: d.get(m.group(1), '${%s}' % m.group(1)), data)
+    #while set(TOKEN.findall(data)) & set(d.keys()):
+    #    d = yaml.load(data)
+    #    data = TOKEN.sub(lambda m: d.get(m.group(1), '${%s}' % m.group(1)), data)
+    new_d = env_replace(d)
 
-    return d
+    return new_d
 
 
 def instrument_code(env):
     print(fc.yellow('Rebuilding executable with instrumentation'))
-    run('cp ~/pat/instrument_coupler.apa ${expdir}/exec/')
-    run('pat_build -O ${expdir}/exec/instrument_coupler.apa -o ${executable}+apa ${executable}')
-    run("sed -i.bak -r -e 's/^export executable=.*$/export executable=\$\{expdir\}\/exec\/fms_mom4p1_coupled.x+apa/g' NAMELIST.environment")
+    run('cp ~/pat/instrument_coupler.apa {expdir}/exec/'.format(**env))
+    run('pat_build -O {expdir}/exec/instrument_coupler.apa -o {executable}+apa {executable}'.format(**env))
+    #TODO: check if this works...
+    env['executable'] =  ''.join((env['executable'], '+apa'))
+#    run("sed -i.bak -r -e 's/^export executable=.*$/export executable=\$\{expdir\}\/exec\/fms_mom4p1_coupled.x+apa/g' NAMELIST.environment")
 
 
 def run_model(env):
     print(fc.yellow('Running model'))
-    run('. run_g4c_model.cray cold 2007010100 2007011000 2007011000 48 rodteste')
+    with cd('{root}/MOM4p1/exp/cpld2.1'.format(**env)):
+        run('. run_g4c_model.cray cold 2007010100 2007011000 2007011000 48 rodteste')
 
 
 def prepare_expdir(env):
     print(fc.yellow('Preparing expdir'))
-    run('cp -R tables ${expdir}')
-    run('touch {0[expdir]}/tables/data_override'.format(env))
+    with cd(env['expdir']):
+        run('cp -R {root}/MOM4p1/exp/cpld2.1/tables {expdir}'.format(**env))
+        run('touch {expdir}/tables/data_override'.format(**env))
 
 
 def prepare_workdir(env):
     print(fc.yellow('Preparing workdir'))
-    run('mkdir -p ${workdir}')
-    run('cp -R $ARCHIVE_OCEAN/database/work20070101/* ${workdir}')
+    run('mkdir -p {workdir}'.format(**env))
+    run('cp -R $ARCHIVE_OCEAN/database/work20070101/* {workdir}'.format(**env))
     run('touch ${workdir}/time_stamp.restart')
 
 
@@ -98,7 +133,7 @@ def check_code(env):
         run('rm -rf %s' % env['code_dir'])
     if not exists(env['code_dir']):
         print(fc.yellow("Creating new repository"))
-        run('hg clone %s %s' % (env['code_repo'], env['code_dir']))
+        run('hg clone {code_repo} {code_dir}'.format(**env))
     with cd(env['code_dir']):
         print(fc.yellow("Updating existing repository"))
         run('hg pull')
@@ -107,11 +142,13 @@ def check_code(env):
 
 
 def link_agcm_inputs(env):
-    run('mkdir -p {0[rootexp]}/AGCM-1.0/model'.format(env))
-    if not exists('${rootexp}/AGCM-1.0/model/datain'):
+    run('mkdir -p {rootexp}/AGCM-1.0/model'.format(**env))
+    if not exists('{rootexp}/AGCM-1.0/model/datain'.format(**env)):
         print(fc.yellow("Linking AGCM input data"))
-        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain ${rootexp}/AGCM-1.0/model/datain')
+        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain {rootexp}/AGCM-1.0/model/datain'.format(**env))
 
 
-def fix_submit():
-    run("sed -i.bak -r -e 's/^aprun -n \$\{npes\} submit/aprun -n \$\{npes\} .\/submit/g' run_g4c_model.cray")
+def fix_submit(env):
+    with cd('{root}/MOM4p1/exp/cpld2.1'.format(**env)):
+        run("sed -i.bak -r -e 's/aprun -n \$\{npes\} submit/aprun -n \$\{npes\} .\/submit/g' run_g4c_model.cray")
+        run("sed -i.bak -r -e 's/\. \.\/NAMELIST/\#\. \.\/NAMELIST/g' run_g4c_model.cray")
