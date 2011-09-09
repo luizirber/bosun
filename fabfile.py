6b6daa6be873fcdc6c2dd5db756c23ae2469daa9
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-09-09 14:30:35 -0300

    Decorator approach didn't work, checking for environ at all functions.

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,6 @@ from fabric.api import run, local, cd, settings, show, prefix
 import fabric.colors as fc
 from fabric.contrib.files import exists, comment
 from fabric.decorators import hosts
-
 import yaml
 
 
@@ -17,9 +16,10 @@ def shell_env(args):
 
 
 @hosts('tupa')
-def deploy(filename='namelist.yaml', instrument=False):
+def deploy(environ=None, filename='namelist.yaml', instrument=False):
     print(fc.green("Started"))
-    environ = _read_config(filename)
+    if not environ:
+        environ = _read_config('namelist.yaml')
     check_code(environ)
     if instrument:
         with prefix('module load perftools'):
@@ -36,7 +36,6 @@ def deploy(filename='namelist.yaml', instrument=False):
 
 
 def _read_config(filename):
-
 
     def rec_replace(env, key, value):
         finder = re.compile('\$\{(\w*)\}')
@@ -65,8 +64,6 @@ def _read_config(filename):
                 new_env[k] = rec_replace(old_env, k, old_env[k])
         return new_env
 
-
-
     data = open(filename, 'r').read()
     TOKEN = re.compile(r'''\$\{(.*?)\}''')
 
@@ -80,77 +77,95 @@ def _read_config(filename):
     return new_d
 
 
-def instrument_code(env):
+def instrument_code(environ=None):
     print(fc.yellow('Rebuilding executable with instrumentation'))
-    run('cp ~/pat/instrument_coupler.apa {expdir}/exec/'.format(**env))
-    run('pat_build -O {expdir}/exec/instrument_coupler.apa -o {executable}+apa {executable}'.format(**env))
-    env['executable'] =  ''.join((env['executable'], '+apa'))
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    run('cp ~/pat/instrument_coupler.apa {expdir}/exec/'.format(**environ))
+    run('pat_build -O {expdir}/exec/instrument_coupler.apa -o {executable}+apa {executable}'.format(**environ))
+    env['executable'] =  ''.join((environ['executable'], '+apa'))
 #    run("sed -i.bak -r -e 's/^export executable=.*$/export executable=\$\{expdir\}\/exec\/fms_mom4p1_coupled.x+apa/g' NAMELIST.environment")
 
 
-def run_model(env):
+def run_model(environ=None):
     print(fc.yellow('Running model'))
-    with shell_env(env):
-        with cd('{root}/MOM4p1/exp/cpld2.1'.format(**env)):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    with shell_env(environ):
+        with cd('{root}/MOM4p1/exp/cpld2.1'.format(**environ)):
             run('. run_g4c_model.cray cold 2007010100 2007011000 2007011000 48 rodteste')
 
 
-def prepare_expdir(env):
+def prepare_expdir(environ=None):
     print(fc.yellow('Preparing expdir'))
-    with cd(env['expdir']):
-        run('cp -R {root}/MOM4p1/exp/cpld2.1/tables {expdir}'.format(**env))
-        run('touch {expdir}/tables/data_override'.format(**env))
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    with cd(environ['expdir']):
+        run('cp -R {root}/MOM4p1/exp/cpld2.1/tables {expdir}'.format(**environ))
+        run('touch {expdir}/tables/data_override'.format(**environ))
 
 
-def prepare_workdir(env):
+def prepare_workdir(environ=None):
     print(fc.yellow('Preparing workdir'))
-    run('mkdir -p {workdir}'.format(**env))
-    run('cp -R $ARCHIVE_OCEAN/database/work20070101/* {workdir}'.format(**env))
-    run('touch {workdir}/time_stamp.restart'.format(**env))
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    run('mkdir -p {workdir}'.format(**environ))
+    run('cp -R $ARCHIVE_OCEAN/database/work20070101/* {workdir}'.format(**environ))
+    run('touch {workdir}/time_stamp.restart'.format(**environ))
 
 
-def clean_model_compilation(env):
+def clean_model_compilation(environ=None):
     print(fc.yellow("Cleaning code dir"))
-    with shell_env(env):
-        with cd(env['expdir']):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    with shell_env(environ):
+        with cd(environ['expdir']):
             with cd('exec'):
-                with prefix('source {root}/MOM4p1/bin/environs.{comp}'.format(**env)):
-                    run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi clean'.format(**env))
+                with prefix('source {root}/MOM4p1/bin/environs.{comp}'.format(**environ)):
+                    run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi clean'.format(**environ))
 
 
-def compile_model(env):
+def compile_model(environ=None):
     print(fc.yellow("Compiling code"))
-    run('mkdir -p {expdir}/exec'.format(**env))
-    with shell_env(env):
-        with cd(env['expdir']):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    run('mkdir -p {expdir}/exec'.format(**environ))
+    with shell_env(environ):
+        with cd(environ['expdir']):
             with cd('exec'):
                 #TODO: generate RUNTM and substitute
-                with prefix('source {root}/MOM4p1/bin/environs.{comp}'.format(**env)):
-                    run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi'.format(**env))
+                with prefix('source {root}/MOM4p1/bin/environs.{comp}'.format(**environ)):
+                    run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi'.format(**environ))
 
 
-def check_code(env):
+def check_code(environ=None):
     print(fc.yellow("Checking code"))
-    if env['clean_checkout']:
-        run('rm -rf {code_dir}'.format(**env))
-    if not exists(env['code_dir']):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    if environ['clean_checkout']:
+        run('rm -rf {code_dir}'.format(**environ))
+    if not exists(environ['code_dir']):
         print(fc.yellow("Creating new repository"))
-        run('hg clone {code_repo} {code_dir}'.format(**env))
-    with cd(env['code_dir']):
+        run('hg clone {code_repo} {code_dir}'.format(**environ))
+    with cd(environ['code_dir']):
         print(fc.yellow("Updating existing repository"))
         run('hg pull')
-        run('hg update {code_branch}'.format(**env))
+        run('hg update {code_branch}'.format(**environ))
 #        run('hg update -r{revision}'.format(**env))
 
 
-def link_agcm_inputs(env):
-    run('mkdir -p {rootexp}/AGCM-1.0/model'.format(**env))
-    if not exists('{rootexp}/AGCM-1.0/model/datain'.format(**env)):
+def link_agcm_inputs(environ=None):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    run('mkdir -p {rootexp}/AGCM-1.0/model'.format(**environ))
+    if not exists('{rootexp}/AGCM-1.0/model/datain'.format(**environ)):
         print(fc.yellow("Linking AGCM input data"))
-        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain {rootexp}/AGCM-1.0/model/datain'.format(**env))
+        run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain {rootexp}/AGCM-1.0/model/datain'.format(**environ))
 
 
-def fix_submit(env):
-    with cd('{root}/MOM4p1/exp/cpld2.1'.format(**env)):
+def fix_submit(environ=None):
+    if not environ:
+        environ = _read_config('namelist.yaml')
+    with cd('{root}/MOM4p1/exp/cpld2.1'.format(**environ)):
         run("sed -i.bak -r -e 's/aprun -n \$\{npes\} submit/aprun -n \$\{npes\} .\/submit/g' run_g4c_model.cray")
         run("sed -i.bak -r -e 's/\. \.\/NAMELIST/\#\. \.\/NAMELIST/g' run_g4c_model.cray")
