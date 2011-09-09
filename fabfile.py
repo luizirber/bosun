f9588f5c543cef514f12b96f85d2d3c2403d4ee3
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-09-09 17:07:51 -0300

    Nailed the decorator. Removed environ checks at all tasks.
        * The new decorator, env_options, try to read a expdir and filename for
          the namelist from the arguments of the task. If it can't be found then
          it try to load a default. Next thing to do is check consistency of the
          environ passed around by the tasks.
        * Declared all tasks with @tasks decorator from Fabric.
        * Removed namelist.yaml, leave only the one in expdir.

--- fabfile.py
+++ fabfile.py
@@ -2,24 +2,44 @@
 
 from __future__ import with_statement
 import re
+import os.path
 
 from fabric.api import run, local, cd, settings, show, prefix
 import fabric.colors as fc
 from fabric.contrib.files import exists, comment
-from fabric.decorators import hosts
+from fabric.decorators import task
 import yaml
 
+
+class NoEnvironmentSetException(Exception):
+    pass
+
+
+def env_options(f):
+    def _wrapped_env(*args, **kw):
+        try:
+            environ = kw['environ']
+            del kw['environ']
+        except KeyError:
+            expdir = kw.get('expdir', 'exp')
+            filename = kw.get('filename', 'namelist.yaml')
+            environ = _read_config(os.path.join(expdir, filename))
+
+        if environ is None:
+            raise NoEnvironmentSetException
+        else:
+            return f(environ=environ, **kw)
+
+    return _wrapped_env
 
 def shell_env(args):
     env_vars = " ".join(["=".join((key, str(value))) for (key, value) in args.items()])
     return prefix("export %s" % env_vars)
 
-
-@hosts('tupa')
-def deploy(environ=None, filename='namelist.yaml', instrument=False):
+@env_options
+@task
+def deploy(environ=None, instrument=False, **kwargs):
     print(fc.green("Started"))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     check_code(environ)
     if instrument:
         with prefix('module load perftools'):
@@ -77,47 +97,47 @@ def _read_config(filename):
     return new_d
 
 
-def instrument_code(environ=None):
+@env_options
+@task
+def instrument_code(environ=None, **kwargs):
     print(fc.yellow('Rebuilding executable with instrumentation'))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     run('cp ~/pat/instrument_coupler.apa {expdir}/exec/'.format(**environ))
     run('pat_build -O {expdir}/exec/instrument_coupler.apa -o {executable}+apa {executable}'.format(**environ))
     env['executable'] =  ''.join((environ['executable'], '+apa'))
 #    run("sed -i.bak -r -e 's/^export executable=.*$/export executable=\$\{expdir\}\/exec\/fms_mom4p1_coupled.x+apa/g' NAMELIST.environment")
 
 
-def run_model(environ=None):
+@env_options
+@task
+def run_model(environ=None, **kwargs):
     print(fc.yellow('Running model'))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     with shell_env(environ):
         with cd('{root}/MOM4p1/exp/cpld2.1'.format(**environ)):
             run('. run_g4c_model.cray cold 2007010100 2007011000 2007011000 48 rodteste')
 
 
-def prepare_expdir(environ=None):
+@env_options
+@task
+def prepare_expdir(environ=None, **kwargs):
     print(fc.yellow('Preparing expdir'))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     with cd(environ['expdir']):
         run('cp -R {root}/MOM4p1/exp/cpld2.1/tables {expdir}'.format(**environ))
         run('touch {expdir}/tables/data_override'.format(**environ))
 
 
-def prepare_workdir(environ=None):
+@env_options
+@task
+def prepare_workdir(environ=None, **kwargs):
     print(fc.yellow('Preparing workdir'))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     run('mkdir -p {workdir}'.format(**environ))
     run('cp -R $ARCHIVE_OCEAN/database/work20070101/* {workdir}'.format(**environ))
     run('touch {workdir}/time_stamp.restart'.format(**environ))
 
 
-def clean_model_compilation(environ=None):
+@env_options
+@task
+def clean_model_compilation(environ=None, **kwargs):
     print(fc.yellow("Cleaning code dir"))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     with shell_env(environ):
         with cd(environ['expdir']):
             with cd('exec'):
@@ -125,10 +145,10 @@ def clean_model_compilation(environ=None):
                     run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi clean'.format(**environ))
 
 
-def compile_model(environ=None):
+@env_options
+@task
+def compile_model(environ=None, **kwargs):
     print(fc.yellow("Compiling code"))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     run('mkdir -p {expdir}/exec'.format(**environ))
     with shell_env(environ):
         with cd(environ['expdir']):
@@ -138,10 +158,10 @@ def compile_model(environ=None):
                     run('make -f {root}/CPLD2.1/source/Make_cpldp1.pgi'.format(**environ))
 
 
-def check_code(environ=None):
+@env_options
+@task
+def check_code(environ=None, **kwargs):
     print(fc.yellow("Checking code"))
-    if not environ:
-        environ = _read_config('namelist.yaml')
     if environ['clean_checkout']:
         run('rm -rf {code_dir}'.format(**environ))
     if not exists(environ['code_dir']):
@@ -154,18 +174,18 @@ def check_code(environ=None):
 #        run('hg update -r{revision}'.format(**env))
 
 
-def link_agcm_inputs(environ=None):
-    if not environ:
-        environ = _read_config('namelist.yaml')
+@env_options
+@task
+def link_agcm_inputs(environ=None, **kwargs):
     run('mkdir -p {rootexp}/AGCM-1.0/model'.format(**environ))
     if not exists('{rootexp}/AGCM-1.0/model/datain'.format(**environ)):
         print(fc.yellow("Linking AGCM input data"))
         run('ln -s $SUBMIT_OCEAN/database/AGCM-1.0/model/datain {rootexp}/AGCM-1.0/model/datain'.format(**environ))
 
 
-def fix_submit(environ=None):
-    if not environ:
-        environ = _read_config('namelist.yaml')
+@env_options
+@task
+def fix_submit(environ=None, **kwargs):
     with cd('{root}/MOM4p1/exp/cpld2.1'.format(**environ)):
         run("sed -i.bak -r -e 's/aprun -n \$\{npes\} submit/aprun -n \$\{npes\} .\/submit/g' run_g4c_model.cray")
         run("sed -i.bak -r -e 's/\. \.\/NAMELIST/\#\. \.\/NAMELIST/g' run_g4c_model.cray")
