a6ff4fc60674663861ced9796bc5e327a07abbf5
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-09-14 12:54:35 -0300

    Add new task to test compilation

--- fabfile.py
+++ fabfile.py
@@ -20,3 +20,15 @@ def deploy(environ, instrument=False, **kwargs):
     link_agcm_inputs(environ)
     prepare_workdir(environ)
     run_model(environ)
+
+
+@env_options
+@task
+def test_compilation(environ, instrument=False, **kwargs):
+    print(fc.green("Started"))
+    prepare_expdir(environ)
+    check_code(environ)
+    if instrument:
+        instrument_code(environ)
+    else:
+        compile_model(environ)
