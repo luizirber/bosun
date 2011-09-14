1171aef44bde6ac19e3874b9bdf4e926bec79a37
Luiz Irber <luiz.irber@cptec.inpe.br>
2011-09-14 11:43:00 -0300

    Explicit import of tasks in fabfile, allowing them to be run via fab command.

--- fabfile.py
+++ fabfile.py
@@ -3,19 +3,20 @@
 import fabric.colors as fc
 from fabric.decorators import task
 
-import tasks
+from tasks import prepare_expdir, check_code, instrument_code, compile_model
+from tasks import link_agcm_inputs, prepare_workdir, run_model, env_options
 
 
-@tasks.env_options
+@env_options
 @task
 def deploy(environ, instrument=False, **kwargs):
     print(fc.green("Started"))
-    tasks.prepare_expdir(environ)
-    tasks.check_code(environ)
+    prepare_expdir(environ)
+    check_code(environ)
     if instrument:
-        tasks.instrument_code(environ)
+        instrument_code(environ)
     else:
-        tasks.compile_model(environ)
-    tasks.link_agcm_inputs(environ)
-    tasks.prepare_workdir(environ)
-    tasks.run_model(environ)
+        compile_model(environ)
+    link_agcm_inputs(environ)
+    prepare_workdir(environ)
+    run_model(environ)
