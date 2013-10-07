d45bb4f171d3e73114e0e9ea637d777df051277e
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-10-07 08:47:19 -0300

    bugfix, zip_safe must be false, so bosun can find the fabfiles

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['Fabric>=1.6.0', 'PyYAML', 'mom-utils', 'python-dateutil']
 
 setup(
     name='bosun',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/luizirber/bosun',
     download_url='https://github.com/luizirber/bosun',
     license='PSF',
@@ -23,7 +23,7 @@ setup(
     author_email='luiz.irber@gmail.com, castelao@gmail.com',
     description='',
     long_description=long_desc,
-    zip_safe=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
