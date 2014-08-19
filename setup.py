dc9f6648088b0b5b558b0ad5d93f4649246c4c0f
Luiz Irber <luiz.irber@gmail.com>
2014-08-19 15:51:38 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
 
 setup(
     name='bosun',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/luizirber/bosun',
     download_url='https://github.com/luizirber/bosun',
     license='PSF',
